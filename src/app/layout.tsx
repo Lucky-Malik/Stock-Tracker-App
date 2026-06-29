7f94d991dbb339f7f2fa1150da3c9de0e92fcb66
Lucky-Malik <lokeshmalik2910@gmail.com>
2026-06-29 10:30:23 +0530

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Signalist",
+  description: "Track real-time stock prices, get personalised alerts and explore detailed company insights",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className="dark:bg-slate-900 dark:text-slate-50"
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
