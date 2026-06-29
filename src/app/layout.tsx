e7e9842038a0604be8e2c703c7c22014e2b49857
Lucky-Malik <lokeshmalik2910@gmail.com>
2026-06-29 12:09:44 +0530

    implemented header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className="dark:bg-slate-900 dark:text-slate-50"
+      className="dark dark:bg-slate-900 dark:text-slate-50"
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
